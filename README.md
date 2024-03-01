27f767e054841e1cf340438ab030ca8b54c0d534
Catherine Walsh <cwalsh7894@gmail.com>
2024-03-01 12:39:53 -0500

    add FC introspection and general plotting functions, update readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
+# sfim_lib
+
 Functionality to be shared across SFIM users and to be made publicly available.
+
+## Functions
+
+Currently this package contains 4 main modules:
+
+- `atlases` : functions for working with the Schafer atlas
+- `fc_introspection` : functions and variables specifically for working with the FC Introspection project, including directory structure, loading data, SYNCQ information.
+- `io` : functions for loading in data
+- `plotting` : functions for plotting data. Specifically, contains functions for plotting FC matrix data.
+
+Other modules may be added as new functionality is developed.
+
+## Installation
+
+Currently, the easiest way to utilize these functions is to use the pip developers mode. To utilize this functionality, clone this repo to wherever you would like to use it (either locally or on Biowulf). From there, navigate into to that cloned directory and pip install:
+
+`pip install -e .`
+
+After running this command, you can import the functions as you would for any Python package. By installing using the developer's editable mode, if you change any of the functions in the package and save them as you normally would, all you have to do is re-import the package in Python for them to be applied.
+
+For example, to import functions and variables from the `fc_introspection` module, one would use the command:
+`from sfim_lib.fc_introspection.initialize_variables import *` - this command will import all necessary variables and directory structures for this project, in addition to the function for loading subject data.
+
+## Dependencies
+
+This package has been tested on Python version 3.12.2. Previous verions of Python (and the packages below) may still work, they just have not been tested.
+
+The primary package dependencies needed to run this code are:
+
+- pandas 2.1.4
+- numpy 1.26.4
+- os
+
+The plotting functionality additionally requires:
+
+- bokeh 3.3.4
+- nilearn 0.10.3
+- holoviews 1.18.3
+- hvplot 0.9.2
+- matplotlib 3.8.0
+- networkx 3.1
+- nxviz 0.7.4
+  - this package seems to be particularly problematic with some deprecation, but there is fucntionality in this submodule that requires this package. Eventually, this will be updated so this issue is fixed
+- scikit-learn 1.4.1.post1
+
+## Contributing
+
+If you develop a function that you think will be useful for the rest of the lab, the best way to integrate them is to create a new branch and submit a pull request. Please see [instructions on the lab documentation](https://github.com/nimh-sfim/lab-docs/blob/main/editing.md) for detailed instructions on how to do this.
---
+++ src/sfim_lib/fc_introspection/initialize_variables.py
@@ -0,0 +1,166 @@
+import os.path as osp
+import pandas as pd
+
+# SNYCQ Information
+SNYCQ_Questions = {'Positive':'I thought about something positive',
+                    'Negative':'I thought about something negative', 
+                    'Future':'I thought about future events',
+                    'Past':'I thought about past events',
+                    'Myself':'I thought about myself',
+                    'People':'I thought about other people',
+                    'Surroundings':'I thought about my present environment/surrounding',
+                    'Vigilance':'I was completely awake',
+                    'Images':'My thoughts were in the form of images',
+                    'Words':'My thoughts were in the form of words',
+                    'Specific':'My thoughts were more specific than vague',
+                    'Intrusive':'My thoughts were intrusive'}
+
+SNYCQ_Question_type = {'Positive': 'Content',
+                       'Negative': 'Content',
+                       'Future': 'Content',
+                       'Past': 'Content',
+                       'Myself': 'Content',
+                       'People': 'Content',
+                       'Surroundings': 'Content',
+                       'Vigilance': 'Misc',
+                       'Images': 'Form',
+                       'Words': 'Form',
+                       'Specific': 'Form',
+                       'Intrusive': 'Form'}
+
+# Top of directory structure
+BIOWULF_SHARE_FOLDER = '/data/SFIMJGC_Introspec/'
+PRJ_DIR              = osp.join(BIOWULF_SHARE_FOLDER,'2023_fc_introspection')
+DATA_DIR             = osp.join(BIOWULF_SHARE_FOLDER,'pdn')
+
+# Folders with original data (fmri and behav)
+ORIG_FMRI_DIR   = '/data/DSST/MPI_LEMON/ds000221-download/'
+ORIG_BEHAV_DIR  = osp.join(PRJ_DIR,'downloads','behavioral')
+
+# Code folders
+NOTEBOOKS_DIR           = osp.join(PRJ_DIR,'code','fc_introspection','notebooks')
+SCRIPTS_DIR             = osp.join(PRJ_DIR,'code','fc_introspection','bash')
+PREPROCESSING_NOTES_DIR = osp.join(PRJ_DIR,'code','fc_introspection','resources','preprocessing_notes')
+RESOURCES_DINFO_DIR     = osp.join(PRJ_DIR,'code','fc_introspection','resources','dataset_info')
+PROC_SNYCQ_DIR          = osp.join(PRJ_DIR,'data','snycq')
+RESOURCES_SNYCQ_DIR     = osp.join(PRJ_DIR,'code','fc_introspection','resources','snycq')
+RESOURCES_NBS_DIR       = osp.join(PRJ_DIR,'code','fc_introspection','resources','nbs')
+RESOURCES_CPM_DIR       = osp.join(PRJ_DIR,'code','fc_introspection','resources','cpm')
+RESOURCES_CONN_DIR      = osp.join(PRJ_DIR,'code','fc_introspection','resources','conn')
+RESOURCES_NIMARE_DIR    = osp.join(PRJ_DIR,'code','fc_introspection','resources','nimare')
+
+# QA Configuration
+FINAL_NUM_VOLS     = 652 # Number of volumes in fully pre-processed runs
+REL_MOT_THRESHOLD  = 0.2 # Maximum Relative Displacement (in mm)
+MAX_CENSOR_PERCENT = 30  # Maximum percentage of censored volumes accepted per scan
+
+
+CPM_NITERATIONS      = 100       # Number of iterations on real data (to evaluate robustness against fold generation)
+CPM_NULL_NITERATIONS = 10000     # Number of iterations used to build a null distribution
+
+# Paths to files we use often
+# ===========================
+ORIG_SNYCQ_PATH      = osp.join(PROC_SNYCQ_DIR, 'SNYCQ_Preproc.csv')                # Files with all SNYCQ answers compiled into a single dataframe
+SBJS_WITH_SNYCQ_PATH = osp.join(RESOURCES_DINFO_DIR,'NC_withSNYCQ_subjects.txt')    # List of subjects with at least one rest scan with SNYCQ
+SNYCQ_CLUSTERS_INFO_PATH =  osp.join(RESOURCES_SNYCQ_DIR,'SNYCQ_clusters_info.csv') # Information with scan membership for each cluster.
+SNYCQ_W_PATH             =  osp.join(RESOURCES_SNYCQ_DIR,'SNYCQ_W.csv')             # W: output from applying sbcNMMF to SNYCQ
+SNYCQ_Q_PATH             =  osp.join(RESOURCES_SNYCQ_DIR,'SNYCQ_Q.csv')             # Q: output from applying sbcNMMF to SNYCQ
+ANAT_PATHINFO_PATH   = osp.join(RESOURCES_DINFO_DIR,'NC_anat_info.csv')             # Where to find anatomical for each subject (sess-01 or sess-02)
+BAD_STPROC_LIST_PATH = osp.join(PREPROCESSING_NOTES_DIR, 'NC_struct_fail_list.csv') # List of scans that failed structural pre-processing
+BAD_FNPROC_LIST_PATH = osp.join(PREPROCESSING_NOTES_DIR, 'NC_func_fail_list.csv')   # List of scans that failed functional pre-processing
+BAD_MOTION_LIST_PATH = osp.join(PREPROCESSING_NOTES_DIR,'NC_func_too_much_motion_list.csv') # List of scans with excessive motion
+
+# Parcellation Configuration
+# ==========================
+ATLASES_DIR            = osp.join(PRJ_DIR,'atlases')
+SUBCORTICAL_ATLAS_NAME          = 'aal2'                                       # This atlas only contains definitions for sub-cortical regions
+SUBCORTICAL_ATLAS_PATH          = osp.join(ATLASES_DIR,SUBCORTICAL_ATLAS_NAME) 
+SUBCORTICAL_BRAINNET_NODES_PATH = osp.join(RESOURCES_NBS_DIR,SUBCORTICAL_ATLAS_NAME,'{SUBCORTICAL_ATLAS_NAME}_BrainNet_Nodes.node'.format(SUBCORTICAL_ATLAS_NAME=SUBCORTICAL_ATLAS_NAME))
+
+CORTICAL_200ROI_ATLAS_NAME    = 'Schaefer2018_200Parcels_7Networks'          # This atlas only contains definitions for cortical regions
+CORTICAL_200ROI_ATLAS_PATH    = osp.join(ATLASES_DIR,CORTICAL_200ROI_ATLAS_NAME)
+FB_200ROI_ATLAS_NAME          = 'Schaefer2018_200Parcels_7Networks_AAL2'     # This atlas is a combination of the cortical and subcortical atlas above
+FB_200ROI_ATLAS_PATH          = osp.join(ATLASES_DIR,FB_200ROI_ATLAS_NAME)
+CORTICAL_200ROI_BRAINNET_NODES_PATH = osp.join(RESOURCES_NBS_DIR,CORTICAL_200ROI_ATLAS_NAME,'{CORTICAL_ATLAS_NAME}_BrainNet_Nodes.node'.format(CORTICAL_ATLAS_NAME=CORTICAL_200ROI_ATLAS_NAME))
+FB_200ROI_BRAINNET_NODES_PATH       = osp.join(RESOURCES_NBS_DIR,FB_200ROI_ATLAS_NAME,      '{FB_ATLAS_NAME}_BrainNet_Nodes.node'.format(FB_ATLAS_NAME=FB_200ROI_ATLAS_NAME))
+
+
+CORTICAL_400ROI_ATLAS_NAME    = 'Schaefer2018_400Parcels_7Networks'          # This atlas only contains definitions for cortical regions
+CORTICAL_400ROI_ATLAS_PATH    = osp.join(ATLASES_DIR,CORTICAL_400ROI_ATLAS_NAME)
+FB_400ROI_ATLAS_NAME          = 'Schaefer2018_400Parcels_7Networks_AAL2'     # This atlas is a combination of the cortical and subcortical atlas above
+FB_400ROI_ATLAS_PATH          = osp.join(ATLASES_DIR,FB_400ROI_ATLAS_NAME)
+CORTICAL_400ROI_BRAINNET_NODES_PATH = osp.join(RESOURCES_NBS_DIR,CORTICAL_400ROI_ATLAS_NAME,'{CORTICAL_ATLAS_NAME}_BrainNet_Nodes.node'.format(CORTICAL_ATLAS_NAME=CORTICAL_400ROI_ATLAS_NAME))
+FB_400ROI_BRAINNET_NODES_PATH       = osp.join(RESOURCES_NBS_DIR,FB_400ROI_ATLAS_NAME,      '{FB_ATLAS_NAME}_BrainNet_Nodes.node'.format(FB_ATLAS_NAME=FB_400ROI_ATLAS_NAME))
+
+def get_sbj_scan_list(when='orig', return_snycq=True):
+    """
+    Get list of scans, subjects and snycq table at different pre-processing stages as selected via the when input.
+    
+    Parameters
+    ----------
+    
+    when : str 
+        can take the following values
+            'orig' --> all available data
+            'post_struct' --> only data that successfully completed the anatomical pre-processing pipeline.
+            'post_funct' --> only data that successfully completed the functional pre-processing pipeline.
+            'post_motion' --> only data that has low motion
+        
+    return_snycq : boolean 
+        flag to select whether or not to return the corresponding sncq table
+    
+    Returns
+    ----------
+    
+    sbj_list: list with subject ids
+    scan_list: list with scan ids as a combination of subject_id and run_id
+    snycq_df: pd.dataframe with SNYCQ answers
+
+    Raises
+    ----------
+    
+    AssertionError : `when` keyword is not an acceptable value for processing. 
+
+
+    """
+    assert when in ['orig','post_struct','post_funct','post_motion'], "++ ERROR: Wrong 'when' argument. Please use one of the following: orig, post_struct, post_funct, post_motion"
+
+    snycq_df = pd.read_csv(ORIG_SNYCQ_PATH)
+    snycq_df = snycq_df.set_index(['Subject','Run'])
+    # Discard subjects that failed structural pre-porcessing
+    # ======================================================
+    if when == 'post_struct':
+        bad_struct_sbj_df   = pd.read_csv(BAD_STPROC_LIST_PATH)
+        bad_struct_sbj_list = list(bad_struct_sbj_df['Subject'].values)
+        snycq_df            = snycq_df.drop(snycq_df.loc[bad_struct_sbj_list,:].index)
+    # Discard subjects that failed functional pre-porcessing
+    # ======================================================
+    if when == 'post_funct':
+        bad_struct_sbj_df   = pd.read_csv(BAD_STPROC_LIST_PATH)
+        bad_struct_sbj_list = list(bad_struct_sbj_df['Subject'].values)
+        snycq_df            = snycq_df.drop(snycq_df.loc[bad_struct_sbj_list,:].index)
+        
+        bad_func_scans_df   = pd.read_csv(BAD_FNPROC_LIST_PATH)
+        bad_func_scans_midx = pd.MultiIndex.from_frame(bad_func_scans_df[['Subject','Run']])
+        snycq_df = snycq_df.drop(bad_func_scans_midx)
+    if when == 'post_motion':
+        bad_struct_sbj_df   = pd.read_csv(BAD_STPROC_LIST_PATH)
+        bad_struct_sbj_list = list(bad_struct_sbj_df['Subject'].values)
+        snycq_df            = snycq_df.drop(snycq_df.loc[bad_struct_sbj_list,:].index)
+        
+        bad_func_scans_df   = pd.read_csv(BAD_FNPROC_LIST_PATH)
+        bad_func_scans_midx = pd.MultiIndex.from_frame(bad_func_scans_df[['Subject','Run']])
+        snycq_df = snycq_df.drop(bad_func_scans_midx)
+        
+        bad_moti_scans_df   = pd.read_csv(BAD_MOTION_LIST_PATH)
+        bad_moti_scans_midx = pd.MultiIndex.from_frame(bad_moti_scans_df[['Subject','Run']])
+        snycq_df = snycq_df.drop(bad_moti_scans_midx)
+    
+    sbj_list  = snycq_df.index.get_level_values('Subject').unique()
+    scan_list = list(snycq_df.index)
+    print ("++ [%s] Number of subjects: %d subjects" % (when,len(sbj_list)))
+    print ("++ [%s] Number of scans:    %d scans"    % (when,len(scan_list)))
+    if return_snycq:
+        return sbj_list, scan_list, snycq_df
+    else:
+        return sbj_list, scan_list
